67563db4d4f21a4f38b8cf01b1ca833eb1d1d699
Jade16 <jade_bortot_paiva@usp.br>
2024-07-14 19:00:41 -0300

    update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -77,11 +77,11 @@
   # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
     # FIXME: Replace with your username
-    your-username = {
+    jade = {
       # TODO: You can set an initial password for your user.
       # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
       # Be sure to change it (using passwd) after rebooting!
-      initialPassword = "correcthorsebatterystaple";
+      #initialPassword = "123456789";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
