809952d280662c54253f41eedd0b421ca8d8181d
Jade16 <jade_bortot_paiva@usp.br>
2024-09-02 22:11:49 -0300

    modifications to use Gnome

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,6 +1,4 @@
-# This is your home-manager configuration file
-# Este eh o arquivo de configuracao do seu home manager
-# O QUE DEVERIA SER COLOCADO AQUI???
+# Home manager settings
 
 {
   inputs,
@@ -10,136 +8,42 @@
   pkgs,
   ...
 }:
+
 {
-  # You can import other home-manager modules here
-  # Voce pode importar outros modulos home-manager aqui
+  # Imports
   imports = [
-    # If you want to use modules your own flake exports (from modules/home-manager):
-    # outputs.homeManagerModules.example
-
-    # Or modules exported from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModules.default
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
   ];
 
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
-      outputs.overlays.additions
-      outputs.overlays.modifications
-      outputs.overlays.unstable-packages
-
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-    };
-  };
-
-  # TODO: Set your username
   home = {
     username = "jade";
     homeDirectory = "/home/jade";
-  };
+  }
 
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
-
-  # Enable home-manager and git
-  programs.home-manager.enable = true;
-  programs.git.enable = true;
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "America/Sao_Paulo";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pt_BR.UTF-8";
-    LC_IDENTIFICATION = "pt_BR.UTF-8";
-    LC_MEASUREMENT = "pt_BR.UTF-8";
-    LC_MONETARY = "pt_BR.UTF-8";
-    LC_NAME = "pt_BR.UTF-8";
-    LC_NUMERIC = "pt_BR.UTF-8";
-    LC_PAPER = "pt_BR.UTF-8";
-    LC_TELEPHONE = "pt_BR.UTF-8";
-    LC_TIME = "pt_BR.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # eu acho que as configs daqui pra baixo poderiam ir na parte de hardware, mas ainda nao sei como organizar as coisas de uma boa forma
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "br";
-    xkbVariant = "";
-  };
-
-  # Configure console keymap
-  console.keyMap = "br-abnt2";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
-    # config = {
-      # common.default = ["gnome"];
-    # };
-  };
-
+  home.packages = with pkgs; [
+    brave
+    kitty
+    sublime
+    capitaine-cursors-themed
+    gnome.gnome-tweaks
+    git
+    gnumake
+    gnat
+    stremio
+    valgrind
+    libsForQt5.okular
+    krita
+    vscode
+    neovim
+    logisim
+    inkscape-with-extensions
+    gimp-with-plugins
+    gdb
+    libsForQt5.kdenlive
+  ];
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
+  # O QUE EH ISSO????
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,5 +1,5 @@
-# This is your system's configuration file.
-# Esse eh o arquivo de configuracoes do sistema
+# General settings for the NixOS System
+# aqui eu vou colocar as configurações do meu os gerais 
 
 {
   inputs,
@@ -11,39 +11,156 @@
 }: 
 
 {
-  # You can import other NixOS modules here
-  # Voce pode importar outros modulos NixOS aqui
+  # Imports
   imports = [
-    # If you want to use modules your own flake exports (from modules/nixos):
-    # Se voce quiser usar modulos que voce mesmo exposta em flakes (de modulos/nixos):
-    # outputs.nixosModules.example
-
-    # Or modules from other flakes (such as nixos-hardware):
-    # Ou modulos de outros flakes (como hardware nixos)
-    # inputs.hardware.nixosModules.common-cpu-amd
-    # inputs.hardware.nixosModules.common-ssd
-
-    # You can also split up your configuration and import pieces of it here:
-    # Voce tambem pode dividir sua configuracao e importar partes dela aqui
-    # ./users.nix
-
-    # Import your generated (nixos-generate-config) hardware configuration
-    # Importe sua configuracao de hardware gerada (nixos-generate-config)
-    # REVER AQUI!!!
+    # Generated hardware configuration (nixos-generate-config)
     ./hardware-configuration.nix
+
+    # Driver configuration
+    ./driver-configuration.nix
   ];
 
+  # Boot
+  boot = {
+    loader = {
+      # O QUE EH ISSO??
+      systemd-boot.enable = true;
+      # O QUE EH ISSO??
+      efi.canTouchEfiVariables = true;
+    };
+  };
 
-  nixpkgs = {
+  # Time Zone
+  time.timeZone = "America/Sao_Paulo";
+
+  # Internationalisation properties
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "pt_BR.UTF-8";
+      LC_IDENTIFICATION = "pt_BR.UTF-8";
+      LC_MEASUREMENT = "pt_BR.UTF-8";
+      LC_MONETARY = "pt_BR.UTF-8";
+      LC_NAME = "pt_BR.UTF-8";
+      LC_NUMERIC = "pt_BR.UTF-8";
+      LC_PAPER = "pt_BR.UTF-8";
+      LC_TELEPHONE = "pt_BR.UTF-8";
+      LC_TIME = "pt_BR.UTF-8";
+    };
+  };
+
+  # Console keymap
+  console.keyMap = "br-abnt2";
+
+  # Enable the X11 windowing system and the GNOME Desktop Environment
+  services = {
+    # O QUE EH ISSO?
+    xserver = {
+      enable = true;
+
+      # Configure keymap in X11
+      layout = "br";
+      xkbVariant = "";
+
+      # O QUE EH ISSO??
+      displayManager = {
+        gdm.enable = true;
+        gnome.enable = true;
+      };
+    };
+
+    # Enable CUPS to print documents
+    printing.enable = true;
+
+    # OpenSSH
+    openssh = {
+      enable = true;
+      settings = {
+        PermitRootLogin = "no";
+        PasswordAuthentication = false;
+      };
+    };
+  };
+
+  # Enable sound with pipewire
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Hardware settings
+  hardware = {
+    # Enable OpenGL
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+  };
+
+  # Enable home-manager and git
+  programs = {
+    home-manager.enable = true;
+    git.enable = true;
+  };
+
+  # Networking
+  networking = {
+    networkmanager.enable = true;
+  };
+
+  # XDG portal settings
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
+    # config = {
+      # common.default = ["gnome"];
+    # };
+  };
+
+  # User settings
+  users.users = {
+    # Username
+    jade = {
+      isNormalUser = true;
+      home = "/home/jade";
+      description = "jade";
+      extraGroups = ["networkmanager" "wheel"];
+      shell = pkgs.zsh;
+      
+      # openssh.authorizedKeys.keys = [
+        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+        # PENDENCIA: Adicione sua chave publica SSH aqui, se voce planeja usar SSH para se conectar
+        # PORQUE E PARA QUE EU PLANEJARIA USAR SSH PARA CONECTAR???
+      # ];
+    };
+  }; 
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Enable experimental features (Flakes)
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  fonts.fontDir.enable = true;
+
+
+
+  #nixpkgs = {
     # You can add overlays here
     # Voce pode adicionar overlays aqui
     # O QUE SAO OVERLAYS???
-    overlays = [
+    #overlays = [
       # Add overlays your own flake exports (from overlays and pkgs dir):
       # Adicione overlays que voce mesmo exporta em flakes (para overlays e pkgs dir):
-      outputs.overlays.additions
-      outputs.overlays.modifications
-      outputs.overlays.unstable-packages
+      #outputs.overlays.additions
+      #outputs.overlays.modifications
+      #outputs.overlays.unstable-packages
 
       # You can also add overlays exported from other flakes:
       # Voce pode tambem adicionar overlays exportadas de outros flakes
@@ -56,97 +173,54 @@
       #     patches = [ ./change-hello-to-hi.patch ];
       #   });
       # })
-    ];
+    #];
     # Configure your nixpkgs instance
     # Configure sua instancia nixpkgs
     # REVER AQUI!!!
-    config = {
+    #config = {
       # Disable if you don't want unfree packages
       # Desabilite se voce nao quiser packages nao gratuitas
-      allowUnfree = true;
-    };
-  };
+      #allowUnfree = true;
+    #};
+  #};
 
-  nix = let
-    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-  in {
-    settings = {
+  #nix = let
+    #flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  #in {
+    #settings = {
       # Enable flakes and new 'nix' command
       # Habilite flakes e novo comando 'nix'
-      experimental-features = "nix-command flakes";
+      #experimental-features = "nix-command flakes";
       # Opinionated: disable global registry
       # Opitativo: desabilite registro global
       # O QUE SAO REGISTROS GLOBAIS EM NIX???
-      flake-registry = "";
+      #flake-registry = "";
       # Workaround for https://github.com/NixOS/nix/issues/9574
       # Gambiarra para https://github.com/NixOS/nix/issues/9574
       # QUAL EH ESSE PROBLEMA???
-      nix-path = config.nix.nixPath;
-    };
+      #nix-path = config.nix.nixPath;
+    #};
     # Opinionated: disable channels
     # Opitativo: desabilite canais
     # O QUE SAO CANAIS EM NIX???
-    channel.enable = false;
+    #channel.enable = false;
 
     # Opinionated: make flake registry and nix path match flake inputs
     # Opitativo: fazer com que o registro do flake e o caminho nix correspondam as entradas do flake
-    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
-  };
+    #registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    #nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  #};
 
   # FIXME: Add the rest of your current configuration
   # FIXE-ME: Adicione o resto da sua configuracao atual
 
   # TODO: Set your hostname
   # PENDENCIA: Defina seu hostname
-  networking.hostName = "jadesnix";
+  #networking.hostName = "jadesnix";
 
-  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
-  # PENDENCIA: Defina as configuracoes de usuario em todo o sistema (grupos, etc) e adicione mais usuarios conforme necessario
-  users.users = {
-    # FIXME: Replace with your username
-    # FIXE-ME: Substitua pelo seu nome de usuario
-    # QUAL A DIFERENCA ENTRE HOSTNAME E USERNAME???
-    jade = {
-      # TODO: You can set an initial password for your user.
-      #PENDENCIA: Voce pode definir uma senha inicial para o seu usuario
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Se fizer isso, voce pode pular a configuracao de uma senha de root passando '--no-root-passwd' para nixos-install
-      # Be sure to change it (using passwd) after rebooting!
-      # Certifique-se de altera-lo (usando passwd) apos reiniciar
-      #initialPassword = "123456789";
-      isNormalUser = true;
-      openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
-        # PENDENCIA: Adicione sua chave publica SSH aqui, se voce planeja usar SSH para se conectar
-        # PORQUE E PARA QUE EU PLANEJARIA USAR SSH PARA CONECTAR???
-      ];
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      # PENDENCIA: Certifique-se de adicionar quaisquer outros grupos necessarios (como networkmanager, audio, docker, etc)
-      # EU USO ALGUM GRUPO??? O QUE E PARA QUE SERVEM ESSES GRUPOS???
-      extraGroups = ["wheel"];
-    };
-  };
+  
 
-  # This setups a SSH server. Very important if you're setting up a headless system.
-  # Isso configura o servidor SSH. Muito importante se você estiver configurando um sistema headless
-  # O QUE EH UM SISTEMA HEADLESS???
-  # Feel free to remove if you don't need it.
-  # Sinta-se a vontade para remover se nao precisar
-  # EU PRECISO???
-  services.openssh = {
-    enable = true;
-    settings = {
-      # Opinionated: forbid root login through SSH.
-      # Opicional: proibir login root atraves de SSH
-      PermitRootLogin = "no";
-      # Opinionated: use keys only.
-      # Opicional: use apenas chaves
-      # Remove if you want to SSH using passwords
-      # Remove se quiser fazer SSH usando senhas
-      PasswordAuthentication = false;
-    };
-  };
+  
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
