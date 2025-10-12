e433b18331e1bcec5cd37fdd1f93221bda6d6463
Jade16 <jade_bortot_paiva@usp.br>
2025-10-11 21:32:47 -0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,116 +1,71 @@
 {
-  description = "My nix config";
+  description = "AAAAAAAAAAAAAAAAAAAAAAAA";
 
   inputs = {
-    # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    # Remova o nixpkgs-unstable se não for usar, ou use-o para pacotes específicos
-    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    #nixpkgs.url = "github:Scrumplex/nixpkgs/nixos-monado";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05"; # <<< Corrigido: vírgula adicionada
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable"; # <<< Corrigido: vírgula adicionada
 
-    # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    # Nix Vim 
     nixvim = {
       url = "github:nix-community/nixvim/nixos-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Package do Zen Browser 
-    zen-browser.url = "github:youwen5/zen-browser-flake"; 
-    zen-browser.inputs.nixpkgs.follows = "nixpkgs";
+    agenix.url = "github:ryantm/agenix"; # <<< Corrigido: vírgula adicionada
 
-    // highlight-start
-    # Adicione o agenix para gerenciamento de segredos
-    agenix.url = "github:ryantm/agenix";
-    // highlight-end
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { 
-    self, 
-    nixpkgs, 
-    home-manager,
-    nixvim,
-    zen-browser,
-    agenix, // highlight-line
-    ...
-  }@inputs: let
-    inherit (self) outputs;
-    systems = [
-      "aarch64-linux"
-      "i686-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-    
-    // highlight-start
-    # Configurações comuns de pkgs para sistemas NixOS
-    commonPkgsConfig = {
-      config = {
-        allowUnfree = true;
-        # permittedInsecurePackages = [ ... ]; # Adicione se necessário
+  outputs = { self, nixpkgs, home-manager, nixvim, agenix, ... } @inputs:
+    let
+      system = "x86_64-linux";
+      lib = nixpkgs.lib;
+      
+      # <<< Estrutura dos módulos corrigida
+      commonModules = [
+        agenix.nixosModules.default
+        home-manager.nixosModules.home-manager
+        nixvim.nixosModules.nixvim
+        # A configuração do home-manager para o usuário Jade foi movida
+        # para o nixosConfigurations, conforme mostrado abaixo.
+      ];
+
+    in {
+      # <<< Seção nixosConfigurations readicionada, que estava faltando
+      nixosConfigurations = {
+        jade-nixos = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./machines/laptop/configuration.nix # <<< Verifique se este caminho está correto!
+          ] ++ commonModules ++ [ # <<< Adicionando os módulos comuns e a config do HM
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.jade = import ./home-manager/home.nix;
+            }
+          ];
+        };
       };
-      overlays = [
-        # Inclua overlays personalizados, como o do zen-browser se disponível
-        # zen-browser.overlays.default 
-      ] ++ (import ./overlays {inherit inputs;});
-    };
-    // highlight-end
-  in {
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
-    overlays = import ./overlays {inherit inputs;};
-    nixosModules = import ./modules/nixos;
 
-    // highlight-start
-    # Módulos comuns para todas as configurações NixOS
-    commonModules = [
-      agenix.nixosModules.default
-      home-manager.nixosModules.home-manager
-      nixvim.nixosModules.nixvim
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        # home-manager.users.jade = import ./home-manager/home.nix; # Mova para a configuração específica da máquina
-      }
-    ];
-    // highlight-end
-
-    nixosConfigurations = {
-      jade-nixos = nixpkgs.lib.nixosSystem {
-        // highlight-start
-        specialArgs = {inherit inputs outputs;};
-        modules = [ 
-          ./nixos/configuration.nix
-        ] ++ commonModules;
-        // highlight-end
-      };
-    };
-
-    homeConfigurations = {
-      "jade@jade-nixos" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [
-          ./home-manager/home.nix
+      nixConfig = {
+        experimental-features = [ "nix-command" "flakes" ];
+        substituters = [ "https://cache.nixos.org/" ];
+        extra-substituters = [
+          "https://ow-mods.cachix.org"
+          "https://nix-community.cachix.org"
+        ];
+        extra-trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCwyvRCYg3Fs="
+          "ow-mods.cachix.org-1:6RTOd1dSRibA2W0MpZHxzT0tw1RzyhKObTPKQJpcrZo="
         ];
       };
     };
-
-    // highlight-start
-    # Configuração do Nix para substituters (cache)
-    nixConfig = {
-      experimental-features = ["nix-command" "flakes"];
-      substituters = [
-        "https://cache.nixos.org/"
-        "https://nix-community.cachix.org"
-      ];
-      trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-    };
-    // highlight-end
-  };
 }
