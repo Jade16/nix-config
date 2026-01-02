866d72bc66ed9b439274156fa33094173b068be7
Jade16 <jade_bortot_paiva@usp.br>
2026-01-02 14:17:53 -0300

    update for version 25.11

--- machines/laptop/configuration.nix
+++ machines/laptop/configuration.nix
@@ -5,27 +5,27 @@
     ./hardware-configuration.nix
 
     # --- Hardware ---
-    ../../modules/config/hardware/keyboard/br-abnt2.nix
-    ../../modules/config/hardware/printer/default.nix
-    ../../modules/config/hardware/scanner/default.nix
-    ../../modules/config/hardware/screen/default.nix
+    #../../modules/config/hardware/keyboard/br-abnt2.nix
+    #../../modules/config/hardware/printer/default.nix
+    #../../modules/config/hardware/scanner/default.nix
+    #../../modules/config/hardware/screen/default.nix
 
     # --- Sistemas ---
-    ../../modules/config/systems/audio/pipewire.nix
+    #../../modules/config/systems/audio/pipewire.nix
     ../../modules/config/systems/bluetooth/default.nix
     ../../modules/config/systems/boot/default.nix
     #../../modules/config/systems/boot/efi.nix
     #../../modules/config/systems/boot/grub.nix 
-    ../../modules/config/systems/desktop/hyprland/default.nix
+    #../../modules/config/systems/desktop/hyprland/default.nix
 
-    ../../modules/config/systems/font/nerd-font.nix
+    #../../modules/config/systems/font/nerd-font.nix
     #../../modules/config/systems/gpu/nvidia.nix
     #../../modules/config/systems/kernel/zen.nix
     ../../modules/config/systems/localization/brazil.nix
     ../../modules/config/systems/network/default.nix
     ../../modules/config/systems/nix/default.nix
-    ../../modules/config/systems/power/management.nix
-    ../../modules/config/systems/power/sleep.nix
+    #../../modules/config/systems/power/management.nix
+    #../../modules/config/systems/power/sleep.nix
     ../../modules/config/systems/services/docker.nix
     ../../modules/config/systems/services/packages/default.nix
     ../../modules/config/systems/services/packages/overlays/overlays.nix
@@ -61,4 +61,48 @@
   environment.systemPackages = with pkgs; [
     papirus-icon-theme
   ];
+
+ ######################################################
+  # XDG portal settings
+  # brilho
+  programs.light.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    wlr = {
+      enable = true;
+      settings = {
+        screencast = {
+          max_fps = 30;
+          chooser_type = "simple";
+          #???
+          chooser_cmd = "${pkgs.slurp}/bin/slurp -f %o -or";
+        };
+      };
+    };
+    config.common.default = "*";
+  };
+  
+  # Security
+  security.polkit.enable = true;
+  security.pam.services.swaylock = {};
+ 
+  systemd.services.docker.environment = {
+    HTTP_PROXY = "http://webproxy.ext.ti.com:80";
+    HTTPS_PROXY = "http://webproxy.ext.ti.com:80";
+    NO_PROXY = "localhost,127.0.0.1";
+  };
+
+
+  # Greeter
+  services.greetd = {
+    enable = true;
+    settings = rec {
+      initial_session = {
+        command = ''${pkgs.zsh}/bin/zsh -c "${pkgs.hyprland}/bin/Hyprland"'';
+        user = "jade";
+      };
+      default_session = initial_session;
+    };
+  };
 }
--- modules/home/systems/users/default.nix
+++ modules/home/systems/users/default.nix
@@ -17,17 +17,46 @@
   #};
 
   imports = [
-    ../../systems/desktop/rofi/default.nix
+    #../../systems/desktop/rofi/default.nix
     ../../systems/desktop/dunst/default.nix
-    ../../systems/desktop/waybar/default.nix
-    ../../systems/desktop/hyprland/default.nix
+    #../../systems/desktop/waybar/default.nix
+    #../../systems/desktop/hyprland/default.nix
     ../../systems/desktop/services-user/cliphist/default.nix
     ../../systems/desktop/services-user/wlsunset/default.nix
-    ../../systems/security/gpg.nix
-    ../../systems/services/packages/default.nix
-    ../../tools/development/git.nix
-    ../../tools/terminal/default.nix
-    ../../tools/terminal/kitty/default.nix
-    ../../tools/terminal/zsh/zsh.nix
+    #../../systems/security/gpg.nix
+    #../../systems/services/packages/default.nix
+    #../../tools/development/git.nix
+    #../../tools/terminal/default.nix
+    #../../tools/terminal/kitty/default.nix
+    #../../tools/terminal/zsh/zsh.nix
   ];
+}
+
+{
+
+  home.sessionPath = ["$HOME/.local/bin"];
+  # Env vars
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    GTK_THEME = "Adwaita:dark";
+  };
+
+  # Direnv
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    enableZshIntegration = true;
+  };
+
+  # System diagostics
+  xdg.configFile."btop/themes/btop.theme".source = ./btop.theme;
+
+  programs.btop = {
+    enable = true;
+    settings = {
+      color_theme = "${config.xdg.configHome}/btop/themes/btop.theme";
+      theme_background = false;
+    };
+  };
+  systemd.user.startServices = "sd-switch";
 }
